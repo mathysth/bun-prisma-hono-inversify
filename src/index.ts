76afdfdada9ad42b866a80df2134d496c6059240
Mathys <mathystheolade973@gmail.com>
2024-01-23 12:04:39 +0100

    Setup cors, csrf

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 import { App } from '@libs/core/server';
+import { AppLogger } from '@libs/core/logger';
 import { Config, ENV_ENUM } from '@config/config';
 import { ControllerRoot } from './controllers';
-import { AppLogger } from '@libs/core/logger';
 import { SERVICE_IDENTIFIER } from '@config/ioc/service-identifier';
+import { cors } from 'hono/cors';
+import { csrf } from 'hono/csrf';
 import { iocContainer } from '@config/ioc/container';
-import { swaggerUI } from '@hono/swagger-ui';
 import { logger } from 'hono/logger';
+import { swaggerUI } from '@hono/swagger-ui';
 
 // Initialize Hono
 const app = iocContainer.get<App>(SERVICE_IDENTIFIER.App).hono;
@@ -18,7 +20,8 @@ config.validateEnv();
 // Initialize Logger
 const appLogger = iocContainer.get<AppLogger>(SERVICE_IDENTIFIER.Logger);
 
-if (config.get<ENV_ENUM>('ENV') === ENV_ENUM.DEV) {
+const env = config.get<ENV_ENUM>('ENV');
+if (env === ENV_ENUM.DEV) {
   // Setup Logger for Hono
   const customLogger = (message: any, ...rest: string[]) => {
     appLogger.pino.info(message, ...rest);
@@ -40,6 +43,21 @@ if (config.get<ENV_ENUM>('ENV') === ENV_ENUM.DEV) {
   });
 }
 
+if (env === ENV_ENUM.PROD) {
+  const origin = ['https://mydomains.com'];
+  app.use('*',
+    cors({
+      origin,
+    })
+  );
+  app.use(
+    '*',
+    csrf({
+      origin,
+    })
+  );
+}
+
 // Setup all routes
 const controllerRoot = iocContainer.get<ControllerRoot>(SERVICE_IDENTIFIER.ControllerRoot);
 controllerRoot.setup();
@@ -47,7 +65,7 @@ controllerRoot.setup();
 // Set the default port to 3000, or use the PORT environment variable
 const port = config.get<number>('PORT');
 
-appLogger.pino.info(`Hono 🥟 GraphQL Server Listening on port ${port}`);
+appLogger.pino.info(`Hono 🥟 Server Listening on port ${port}`);
 
 export default {
   port,
