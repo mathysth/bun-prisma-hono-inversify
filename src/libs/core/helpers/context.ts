a04803c33e460510774d63209d6c08f481ebe73d
Mathys <mathystheolade973@gmail.com>
2024-02-02 00:08:09 +0100

    Fix tests logger
    We throw an error that will be caught by app.catchError and then rendered with pino.error

--- src/libs/core/helpers/context.ts
+++ src/libs/core/helpers/context.ts
@@ -1,10 +1,6 @@
-import { iocContainer } from '@config/ioc/container';
-import { SERVICE_IDENTIFIER } from '@config/ioc/service-identifier';
 import { Context } from 'hono';
-import { AppLogger } from '../logger';
 
 export function isContextDefined(ctx: Context | undefined): void | Error {
   if (ctx) return;
-  const appLogger = iocContainer.get<AppLogger>(SERVICE_IDENTIFIER.Logger);
-  appLogger.pino.error(new Error('Context is not defined'));
+  throw new Error('Context is not defined');
 }
